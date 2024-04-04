0bb872c76e22571c36078b0f3292305a42184f36
Noi-Git <noi.patsin@gmail.com>
2024-04-04 17:38:00 -0400

    create interfaces

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,16 @@
 import { useEffect, useState } from 'react'
 import apiClient from '../services/api-client'
 
+interface Character {
+  id: number
+  name: string
+}
+
+interface FetchCharactersResponse {
+  count: number
+  results: Character[]
+}
+
 const GameGrid = () => {
   const [characters, setCharacters] = useState([])
   const [error, setError] = useState('')
