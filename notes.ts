9c3ca6616d67a2998d5ce39d2993c1d1e0ef6750
Noi-Git <noi.patsin@gmail.com>
2024-04-05 13:28:00 -0400

    create Props interface and using Character interface created in custom hook

---
+++ notes.ts
@@ -0,0 +1,5 @@
+// Mosh's GitHub: https://github.com/mosh-hamedani/game-hub
+
+// Noi's GitHub: https://github.com/Noi-Git/game-hub
+
+// Chakra-UI: https://chakra-ui.com/
---
+++ src/components/Character.tsx
@@ -0,0 +1,11 @@
+import { Character } from '../hooks/useCharacters'
+
+interface Props {
+  character: Character
+}
+
+const Character = () => {
+  return <div>Character</div>
+}
+
+export default Character
