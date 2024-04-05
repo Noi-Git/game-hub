7ed01ebd0eb7091640bd6ed32106162fc3eb9888
Noi-Git <noi.patsin@gmail.com>
2024-04-05 19:01:04 -0400

    render the image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
 const getCroppedImageUrl = (url: string) => {
   const target = 'media/'
-  const index = url.indexOf('target') + 'target'.length
-  return url.slice(0, index) + 'crop/600/400' + url.slice(index)
+  const index = url.indexOf(target) + target.length
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
 }
 
 export default getCroppedImageUrl
+
+//https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg#google_vignette
