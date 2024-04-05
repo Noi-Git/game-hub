26a892370c85a2d0c55881e0e467fc77fe12d6a0
Noi-Git <noi.patsin@gmail.com>
2024-04-05 18:43:38 -0400

    sytle color of the critic score base on game value

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,8 +5,13 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
+  // if score > 75 set color to green
+  // if score > 60 set color to yellow
+  // otherwise do not thing
+  let color = score > 75 ? 'green' : score > 60 ? 'yellow' : ''
+
   return (
-    <Badge fontSize='14px' paddingX={2} borderRadius='4px'>
+    <Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius='4px'>
       {score}
     </Badge>
   )
