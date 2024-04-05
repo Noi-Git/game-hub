0a669a75b6f977cae197b15ed03184c1105156c7
Noi-Git <noi.patsin@gmail.com>
2024-04-05 18:02:31 -0400

    create a new file

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,10 @@
+import { Platform } from '../hooks/useGames'
+
+interface Props {
+  platforms: Platform[]
+}
+const PlatformIconList = ({ platforms }: Props) => {
+  return <div>PlatformIconList</div>
+}
+
+export default PlatformIconList
