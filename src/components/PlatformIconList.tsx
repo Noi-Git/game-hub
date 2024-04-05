c383f6dc9f036eba2f91e74b451e5022309eed55
Noi-Git <noi.patsin@gmail.com>
2024-04-05 18:19:31 -0400

    create iconMap and use it in the card

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,15 +1,38 @@
-import { Text } from '@chakra-ui/react'
-
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaLinux,
+  FaApple,
+  FaAndroid,
+} from 'react-icons/fa'
+import { MdPhoneIphone } from 'react-icons/md'
+import { SiNintendo } from 'react-icons/si'
+import { BsGlobe } from 'react-icons/bs'
+import { Icon, Text } from '@chakra-ui/react'
 import { Platform } from '../hooks/useGames'
 
 interface Props {
   platforms: Platform[]
 }
 const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    // these are call slug
+  }
+
   return (
     <>
       {platforms.map((platform) => (
-        <Text>{platform.name}</Text>
+        <Icon as={iconMap[platform.slug]} />
       ))}
     </>
   )
