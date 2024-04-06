a7530377cc1a93f07d6a057c59da156aa5bf97c4
Noi-Git <noi.patsin@gmail.com>
2024-04-05 20:22:10 -0400

    add loading

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,17 +24,26 @@ const useGames = () => {
 
   const [games, setGames] = useState<Game[]>([])
   const [error, setError] = useState('')
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
+
     apiClient
       .get<FetchGamesResponse>('/games', { signal })
-      .then((res) => setGames(res.data.results))
-      .catch((error) => setError(error.message))
+      .then((res) => {
+        setGames(res.data.results)
+        setLoading(false)
+      })
+      .catch((error) => {
+        setError(error.message)
+        setLoading(false)
+      })
 
     // return controller.abort() //clean up
   }, [])
 
-  return { games, error }
+  return { games, error, isLoading }
 }
 
 export default useGames
