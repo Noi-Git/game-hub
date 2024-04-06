3da41cabf2c986d4d35e74d2db87983dfc1d4ee1
Noi-Git <noi.patsin@gmail.com>
2024-04-05 23:24:13 -0400

    refactor the custom hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,45 +1,10 @@
-import { useEffect, useState } from 'react'
-import { CanceledError } from 'axios'
-import apiClient from '../services/api-client'
+import useData from './useData'
 
 export interface Genre {
   id: number
   name: string
 }
 
-interface FetchGenresResponse {
-  count: number
-  results: Genre[]
-}
-
-const useGenres = () => {
-  const controller = new AbortController()
-  const signal = controller.signal
-
-  const [genres, setGenres] = useState<Genre[]>([])
-  const [error, setError] = useState('')
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-
-    apiClient
-      .get<FetchGenresResponse>('/genres', { signal })
-      .then((res) => {
-        setGenres(res.data.results)
-        setLoading(false)
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) {
-          setError(error.message)
-          setLoading(false)
-        }
-      })
-
-    // return controller.abort() //clean up
-  }, [])
-
-  return { genres, error, isLoading }
-}
+const useGenres = () => useData<Genre>('/genres')
 
 export default useGenres
